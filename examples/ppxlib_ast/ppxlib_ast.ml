9cabb75f03319e26f07f45a707aa90430e4a2fc1
Thierry Martinez <Thierry.Martinez@inria.fr>
2019-05-09 11:53:40 +0200

    Cite transformation rules in comments

--- examples/ppxlib_ast/ppxlib_ast.ml
+++ examples/ppxlib_ast/ppxlib_ast.ml
@@ -1,8 +1,17 @@
-(* This example implements mechanically the transformations rule
+(* This example implements mechanically the transformation rules
    described in the comments of
    https://github.com/ocaml-ppx/ppxlib/blob/master/ast/ast.ml *)
 
+(* "- replacing app [type ...] by [and ...] to make everything one
+recursive block" *)
 [%%recursive
+
+  (* "- adding the type definitions for position, location, loc and longident"
+     They are imported from Stdlib.Lexing.position, Location.t, 'a Location.loc,
+     and Longident.t respectively. *)
+
+  (* "- flattening all the modules"
+     All the modules are imported with module%import. *)
   module%import Stdlib = struct
     module%import Lexing = struct
       type position = _ [@@rewrite]
@@ -10,18 +19,27 @@
   end
 
   module%import Location = struct
+    (* "- renaming a few types:
+        - - Location.t -> location" *)
     type location = _ [@@from: t] [@@rewrite]
 
     type 'a loc = _ [@@rewrite]
   end
 
-  module%import Longident = struct
+  module%import Longident = struc
+    (* "- renaming a few types:
+        - - Longident.t -> longident" *)t
     type longident = _ [@@from: t] [@@rewrite]
   end
 
+  (* "- adding a type longident_loc = longident loc and replacing all
+   the occurences of the latter by the former. This is so that we can
+   override iteration an the level of a longident loc." *)
   type longident_loc = longident loc [@@rewrite]
 
   module%import Asttypes = struct
+    (* "- removing Asttypes.constant (unused and conflicts with
+       Parsetree.constant)" *)
     type constant [@@remove]
 
     type 'a loc [@@rewrite] [@@remove]
